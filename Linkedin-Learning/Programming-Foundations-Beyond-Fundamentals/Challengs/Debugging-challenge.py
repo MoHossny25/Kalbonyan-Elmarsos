28a0f36f4c7f6ade152103dffc5fbf0e2af9263d
Mohamed Hossny <88251274+MoHossny25@users.noreply.github.com>
2021-11-13 02:01:39 +0200

    Update Debugging-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Debugging-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Debugging-challenge.py
@@ -1,1 +1,28 @@
 
+                                     # The code before the wrong modification
+# def plant_recommendation(care):
+#     if care = 'low':
+#         print('aloe')
+#     elif care == 'medium':
+#         print('pothos')
+#     elif care == 'medium':
+#         print('orchid')
+
+# plant_rec('low')
+# plant_recommendation('medium')
+# plant_recommendation('high')
+
+
+                                    # The code after the wrong modification
+def plant_recommendation(care):
+    if care == 'low':
+        print('aloe')
+    elif care == 'medium':
+        print('pothos')
+    elif care == 'medium':
+        print('orchid')
+
+plant_recommendation('low')
+plant_recommendation('medium')
+plant_recommendation('high')
+
