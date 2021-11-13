8e427f3c6eb00319287a42b4c28df45cb267ef72
Mohamed Hossny <88251274+MoHossny25@users.noreply.github.com>
2021-11-13 02:02:19 +0200

    Update Working-with-collections-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,16 @@
 
+star1 = 'sol'
+star2 = 'Alpha Centaur'
+star3 = 'Barnard'
+star4 = 'wolf 359'
+# Hight
+peaks = {
+    'African':'kilimanjaro',
+    'Antarctic':'vinson',
+    'Australian':'punmcak',
+    'Eurasian':'Everest',
+    'Nourth_American':'Denali',
+    'pacific':'mauna Kea',
+    'South_American':'Aconcagua'
+}
+print(f"{star4} \n{peaks['pacific']}")
