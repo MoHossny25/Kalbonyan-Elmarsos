a8c83146af1703bc9ba691059f4f7c90aba1ff7c
Mohamed Hossny <88251274+MoHossny25@users.noreply.github.com>
2021-11-13 02:02:00 +0200

    Update Strings-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Strings-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Strings-challenge.py
@@ -1,1 +1,6 @@
 
+miles = input('Enter a distance in miles: ')
+#kilo_v = miles_v * 1.609344
+miles_float = float(miles )
+Kilometers = miles_float * 1.609344
+print(f"Converting from kilometers to millimeters= {Kilometers}")
